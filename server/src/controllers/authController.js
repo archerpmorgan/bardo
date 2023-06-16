f05699e4f7d8ea5b9710ef6cbcddaa6c0bb3dc6f
Archer Morgan <archermorgan@Archers-MacBook-Pro.local>
2023-06-16 13:57:20 -0700

    more progress

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,8 +1,10 @@
 import { userSchema } from "../utils/mongoClient.js";
-import mongoose, { trusted } from "mongoose";
+import mongoose from "mongoose";
 
 
 // POST /login 
+import bcrypt from "bcrypt";
+
 // attempt to login user
 const postLogin = async (req, res) => {
   res.status(200).json({ data: "some data" });
@@ -11,23 +13,25 @@ const postLogin = async (req, res) => {
 // POST /register
 // attempt to register user
 const postRegister = async (req, res) => {
-  const userRequest = req.body;
-  console.log(userRequest);
   const User = mongoose.model("User", userSchema)
   const user = new User({
-    username: userRequest.username,
-    email: userRequest.email,
-    password: userRequest.password
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hash(req.body.password)
   })
-  // this doesn't work yet
-  try {
-    user.save();
-  }
-  catch (error) {
-    console.log(error);
-  }
-  console.log("tried to save")
-  res.status(200).json({ data: "some data" });
+  user.save()
+    .then(() => {
+      res.status(200).json({ message: "registration succeeded" });
+    })
+    .catch(error => {
+      if (error.code === 11000) {
+        res.status(400).json({ message: "a user already exists with this email" })
+        console.log('Duplicate entry error!');
+      } else {
+        res.status(400).json({ message: "failed to register for an unknown reason" })
+        console.log('An error occurred while saving the model:', error);
+      }
+    })
 };
 
 // Export the controller functions for use in routes
