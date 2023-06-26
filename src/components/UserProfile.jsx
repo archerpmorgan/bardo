c544bf40ade033fd59eebdf980fbb9095b85178b
Archer Morgan <user.name>
2023-06-26 11:43:22 -0400

    more progress

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import { Button, Paper, Typography } from '@mui/material';
 import { useNavigation } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
+import {
+  selectUserProfile
+} from '../redux/slices/userProfileSlice';
 
 export default function UserProfile() {
+
+  const userProfile = useSelector(selectUserProfile);
+  console.log(userProfile);
 
   const homeHandler = () => {
     navigate("/landing");
@@ -12,6 +18,12 @@ export default function UserProfile() {
   return (
     <Paper>
       <Typography>Displaying currently known user profile data</Typography>
+      <Typography>photo: {userProfile.photo}</Typography>
+      <Typography>name: {userProfile.name}</Typography>
+      <Typography>email: {userProfile.email}</Typography>
+      <Typography>userId: {userProfile.userId}</Typography>
+      <Typography>defaultBookendOpenText: {userProfile.defaultBookendOpenText}</Typography>
+      <Typography>defaultBookendCloseText: {userProfile.defaultBookendCloseText}</Typography>
       <HomeButton onPress={homeHandler} text="HOME" />
     </Paper>
   );
